9f4c8e95bd3c85c40fc60c1f48dada7260113fdc
vanix <211905669@qq.com>
2026-05-21 10:03:11 +0800

    03_static_task

---
+++ 03_static_task/MDK-ARM/RTE/_03_StaticTask/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '03_StaticTask' 
+ * Target:  '03_StaticTask' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
