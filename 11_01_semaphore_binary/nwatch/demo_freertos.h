823938cfdbdd53275dc3117726f5ff375a7e9554
vanix <211905669@qq.com>
2026-05-21 10:03:15 +0800

    11_01_semaphore_binary

---
+++ 11_01_semaphore_binary/nwatch/demo_freertos.h
@@ -0,0 +1,33 @@
+#ifndef _DEMO_FREERTOS_H_
+#define _DEMO_FREERTOS_H_
+
+#define IR_KEY_POWER  0xa2
+#define IR_KEY_MENU   0xe2
+#define IR_KEY_TEST   0x22
+#define IR_KEY_ADD    0x02
+#define IR_KEY_RETURN 0xc2
+#define IR_KEY_LEFT   0xe0
+#define IR_KEY_PLAY   0xa8
+#define IR_KEY_RIGHT  0x90
+#define IR_KEY_0      0x68
+#define IR_KEY_DEC    0x98
+#define IR_KEY_C      0xb0
+#define IR_KEY_1      0x30
+#define IR_KEY_2      0x18
+#define IR_KEY_3      0x7a
+#define IR_KEY_4      0x10
+#define IR_KEY_5      0x38
+#define IR_KEY_6      0x5a
+#define IR_KEY_7      0x42
+#define IR_KEY_8      0x4a
+#define IR_KEY_9      0x52
+#define IR_KEY_REPEAT 0x00
+
+
+void demo_freertos_init(void);
+void start_task(void* pvParameters);
+void task1(void* pvParameters);
+void task2(void* pvParameters);
+
+
+#endif 
